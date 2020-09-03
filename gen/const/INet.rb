1f033e43fb98ce457a8c1dd4537fc61f928b1850
pavel <pavel.rosicky@easy.cz>
2020-09-03 02:06:13 +0200

    windows

--- gen/const/INet.rb
+++ gen/const/INet.rb
@@ -1,9 +1,7 @@
 require_relative '../../gen/ConstGenerator'
 def gen_inet_java(options)
   ConstGenerator.new 'platform.inet', options do |cg|
-    cg.include "sys/socket.h"
-    cg.include "sys/types.h"
-    cg.include "netinet/in.h"
+    cg.include IS_WINDOWS ? "Ws2tcpip.h" : "netinet/in.h"
     %w[
       INET_ADDRSTRLEN
     ].each {|c| cg.const c, "%lu", "(unsigned long)"}
--- gen/const/INet6.rb
+++ gen/const/INet6.rb
@@ -1,9 +1,7 @@
 require_relative '../../gen/ConstGenerator'
 def gen_inet6_java(options)
   ConstGenerator.new 'platform.inet6', options do |cg|
-    cg.include "sys/socket.h"
-    cg.include "sys/types.h"
-    cg.include "netinet/in.h"
+    cg.include IS_WINDOWS ? "Ws2tcpip.h" : "netinet/in.h"
     %w[
       INET6_ADDRSTRLEN
     ].each {|c| cg.const c, "%lu", "(unsigned long)"}
